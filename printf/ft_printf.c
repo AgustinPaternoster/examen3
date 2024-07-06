dc71418cf628ed52de3457674a27757b83911ac8
pater <agustinpaternoster@gmail.com>
2024-07-06 18:04:30 +0200

    primeros pasos

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -1,6 +1,49 @@
-#include <stdio.h>
+#include "ft_printf.h"
+
+int ft_putstr(char * str)
+{
+    int i;
+    int total;
+
+    i = 0;
+    total = 0;
+    while (str[i])
+    {
+        if (write(1,&str[i],1) == -1)
+            return (-1);
+        i++;
+        total++;
+    }
+    return (total);
+    
+}
+
+int p_fuctions(va_list arg, char c)
+{
+    
+    return (0);
+}
+
+int ft_printf(char *str, ...)
+{
+    int i;
+    va_list arg;
+    int result;
+
+    va_start(arg, str);
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == '%')
+            result += p_fuctions(arg,str[i++]);
+        i++;
+    }
+    
+
+}
 
 int main(int arc, char **argv)
 {
+    test_variadic(2,"test",123);
     return (0);
 }
--- test.c
+++ test.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+int printt(int n)
+{
+	int i = 0;
+	int r = 3;
+	while (i < n)
+	{
+		printf("%d", i);
+		r++;
+	}
+	return r;
+}
+
+
 int main (void)
 {
-	printf("test");
+	char *str = "123";
+	printf("%c",str[1]);
 }
